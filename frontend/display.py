39c16ade69ec63a849e15c1d5cccf2944f3b7301
SamohtNB <60231047+SamohtNB@users.noreply.github.com>
2025-06-20 14:25:43 +0200

    Refactor display function to page_accueil and update navigation to use sidebar radio

--- frontend/display.py
+++ frontend/display.py
@@ -3,18 +3,16 @@ import pandas as pd
 from signalement import page_signalement
 from map_decharge import page_map_decharge
 
-    
+st.set_page_config(page_title="AI 4 biov", layout="wide")
 
-def display():
-    
-    
+def page_accueil():
     st.title("AI 4 biov")
     st.write("Ceci est la partie frontend du projet")
     
-    menu = st.selectbox(
-        "Choisissez une page",
-        ("Accueil", "Signalement de Décharge Sauvage", "Carte des Décharges Sauvages")
-    )
+    st.sidebar.title("Navigation")
+    
+    menu = st.sidebar.radio("Choisissez une page",
+            ["Accueil", "Carte des Décharges Sauvages", "Signalement de Décharge Sauvage"])
     
     if menu == "Accueil":
         st.markdown("""
@@ -36,4 +34,4 @@ def display():
         page_map_decharge()
 
 if __name__ == "__main__":
-    display()
+    page_accueil()
